87f4700c937939d2bf198944f25cfc617b91b528
Ivan Milisavljevic <ivan.milisavljevic@creitive.com>
2019-08-28 15:35:03 +0200

    add layout for home activity

--- home/src/main/java/io/milis/sixt/home/ui/home/HomeActivity.kt
+++ home/src/main/java/io/milis/sixt/home/ui/home/HomeActivity.kt
@@ -1,17 +1,39 @@
 package io.milis.sixt.home.ui.home
 
 import android.os.Bundle
+import com.mancj.materialsearchbar.MaterialSearchBar
 import io.milis.sixt.core.common.MvpActivity
 import io.milis.sixt.home.R
+import kotlinx.android.synthetic.main.activity_home.*
 
-
-class HomeActivity : MvpActivity(), HomeView {
+class HomeActivity : MvpActivity(), HomeView, MaterialSearchBar.OnSearchActionListener {
 
     override val presenter by presenterProvider(HomePresenter::class.java, this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        inject()
+
+        searchBar.text = "Find cars"
+        searchBar.setCardViewElevation(12)
+        searchBar.setOnSearchActionListener(this)
+    }
+
+    override fun onButtonClicked(buttonCode: Int) {
+        when (buttonCode) {
+            MaterialSearchBar.BUTTON_NAVIGATION -> {
+            }
+            MaterialSearchBar.BUTTON_BACK -> {
+                searchBar.disableSearch()
+            }
+        }
+    }
+
+    override fun onSearchStateChanged(enabled: Boolean) {
+    }
+
+    override fun onSearchConfirmed(text: CharSequence?) {
     }
 
 }
