76f84bc9367b773993b7c099cfdc0c5a2aef924f
Ivan Milisavljevic <cartman.dev@gmail.com>
2019-08-31 17:53:46 +0200

    remove workmanager dependecy from app module

--- app/src/main/java/io/milis/sixt/App.kt
+++ app/src/main/java/io/milis/sixt/App.kt
@@ -1,11 +1,6 @@
 package io.milis.sixt
 
 import android.app.Application
-import android.content.Context
-import androidx.work.Configuration
-import androidx.work.WorkManager
-import io.milis.sixt.core.dagger.CoreComponent
-import io.milis.sixt.core.dagger.DaggerCoreComponent
 import timber.log.Timber
 
 class App : Application() {
